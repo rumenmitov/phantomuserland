a8a5b43261703d90cec64a55ab92d36783aea5fc
anton <antonov9312@yandex.ru>
2021-03-11 18:57:47 +0300

    relevant plc bin + mkjar.sh

--- tools/plc/mkjar.sh
+++ tools/plc/mkjar.sh
@@ -6,5 +6,29 @@ jar cvfe ../../../build/jar/plc.jar ru.dz.plc.PlcMain .
 jar xvf ../lib/soot-2.5.0.jar 
 jar cvfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain .
 
-# cd ../lib
-# jar uvfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain ../lib/soot-2.5.0.jar 
+# # cd ../lib
+# # jar uvfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain ../lib/soot-2.5.0.jar 
+
+
+
+#cd bin
+#jar cfe ../../../build/jar/plc.jar ru.dz.plc.PlcMain .
+
+#jar xf ../lib/soot-2.5.0.jar 
+#jar cfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain .
+
+#jar cvfe ../../../build/jar/plc.jar ru.dz.plc.PlcMain .
+
+#jar xvf ../lib/soot-2.5.0.jar 
+#jar cvfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain .
+
+
+#cd ../lib
+#jar uvfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain ../lib/soot-2.5.0.jar 
+
+# cd bin
+# jar cfe ../../../build/jar/plc.jar ru.dz.plc.PlcMain .
+
+# jar xf ../lib/soot-2.5.0.jar
+# jar cfe ../../../build/jar/jpc.jar ru.dz.soot.SootMain .
+
